6552f905214e5b45902892c2268e033be05496f2
Seb Hein <git.seb@tuta.io>
2023-09-08 20:37:29 -0400

    command name must start with a capital letter

--- plugin/tron.lua
+++ plugin/tron.lua
@@ -5,7 +5,6 @@ end
 
 local cmd = api.nvim_create_user_command
 
-
-cmd('tronRun', function() require('tron').run_test() end, {nargs = 0})
---cmd('tronShow', function() require('tron').show_output() end, {nargs = 0})
-cmd('tronSplit', function() require('tron').run_test_in_split() end, {nargs = 0})
+cmd('TronRun', function() require('tron').run_test() end, {nargs = 0})
+--cmd('TronShow', function() require('tron').show_output() end, {nargs = 0})
+cmd('TronSplit', function() require('tron').run_test_in_split() end, {nargs = 0})
